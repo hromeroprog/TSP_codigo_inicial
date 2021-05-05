9cd0c53229bf690328223371618ae6652588e939
Hugo Romero <100386322@alumnos.uc3m.es>
2021-05-05 18:18:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 12:02:52 2021
+
+@author: 2rome
+"""
+
+from TSP import TSP
+
+if __name__ == '__main__':
+    tsp = TSP() #Crear el objeto
+    fichero = "ulysses16.tsp"
+    #tsp.obtener_desde_archivo_tsp(fichero)  #Lee de un archivo .tsp
+    tsp.obtener_random(12) #Genera un escenario aleatorio de la dimension que le pases
+    tsp.greedy_solve() #Genera una solucion de ruta
+    tsp.draw_with_solution() #dibujar la solucion
+    #tsp.draw() #dibujar solo las ciudades
+    
+    tsp.r_solve() #Genera una solucion de ruta
+    tsp.draw_with_solution() #dibujar la solucion
+    tsp.backtracking_solve()
